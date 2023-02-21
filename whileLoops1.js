d62e18e97f951fc6ed144e88b472148b3daa8b4e
7ezr <7ezr@fresnou.org>
2023-02-21 14:17:54 -0800

    adding while loops pt 1

---
+++ whileLoops1.js
@@ -0,0 +1,33 @@
+const readLine = require('readline-sync');
+//
+// let w = 0;
+// while (w < 26){
+//     console.log(w);
+//     w++
+// }
+//
+// let c = 0;
+// while (c < 51){
+//     console.log(c);
+//     c++
+// }
+//
+// let s = -10;
+// while (s < 11){
+//     console.log(s);
+//     s++
+// }
+//
+// let f = 0;
+// while (f  > -51){
+//     console.log(f);
+//     f--
+// }
+let e = 0;
+while (e < 100) {
+    if (e%2==100){
+        console.log(e);
+    }
+}
+
+
