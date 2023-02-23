5b1147f30fbac5a7b6c4e41feb0f523c99e9377e
7ezr <7ezr@fresnou.org>
2023-02-23 14:19:14 -0800

    adding forLoops and some of forLoops1

---
+++ forLoops1.js
@@ -0,0 +1,14 @@
+//for loops
+
+// for(let i = 0; i<=150; i++){
+//     console.log(i);
+// }
+// for(let i = 0; i<=40; i++){
+//     console.log(i**2);
+// }
+for(let i = 0; i<=30; i++){
+    console.log(i**3);
+}
+if (i%5==0; i<=700; i++){
+    console.log(i);
+}
