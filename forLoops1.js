0ea32194aed5a9153fa0bb6aaa4b5c3f2941c37e
7ezr <7ezr@fresnou.org>
2023-02-24 13:43:56 -0800

    adding forLoops1

--- forLoops1.js
+++ forLoops1.js
@@ -3,12 +3,24 @@
 // for(let i = 0; i<=150; i++){
 //     console.log(i);
 // }
+
 // for(let i = 0; i<=40; i++){
 //     console.log(i**2);
 // }
-for(let i = 0; i<=30; i++){
-    console.log(i**3);
+
+// for(let i = 0; i<=30; i++) {
+//     console.log(i ** 3);
+// }
+
+// for(let i = 0; i<=700; i++) {
+//     if (i % 5 == 0) {
+//         console.log(i);
+//     }
+// }
+
+for(let i = 350; i<=1000; i++) {
+    if (i % 10 == 0) {
+        console.log(i);
+    }
 }
-if (i%5==0; i<=700; i++){
-    console.log(i);
-}
+
